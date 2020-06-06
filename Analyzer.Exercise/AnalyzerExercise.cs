497a0707bfb0f21625ada44ec666cefa0898221a
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-06 18:38:59 +0200

    Final version of analyzer

--- Analyzer.Exercise/AnalyzerExercise.cs
+++ Analyzer.Exercise/AnalyzerExercise.cs
@@ -1,12 +1,4 @@
 public static class AnalyzerExercise
 {
-    public static string Greeting()
-    {
-        return "Hello you!";
-    }
-
-    public static string Greeting(string name)
-    {
-        return "Hello " + name + "!";   
-    }
+    public static string Greeting(string name = "you") => $"Hello {name}!";
 }
