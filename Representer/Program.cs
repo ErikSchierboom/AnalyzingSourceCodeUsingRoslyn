896536a8f3de96fb15e46d819febed6bb602bc8a
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-07 16:19:51 +0200

    Add stubs for representer and test runner

--- Representer/Program.cs
+++ Representer/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
 
 namespace Representer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            MSBuildLocator.RegisterDefaults();
+
+            var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync(@"C:\Programmeren\AnalyzingSourceCodeUsingRoslyn\Representer.Exercise\Representer.Exercise.csproj");
+            var exercise = project.Documents.Single(document => document.Name == "RepresenterExercise.cs");
+
+            var representation = await exercise.GetSyntaxRootAsync();
+            
             Console.WriteLine("Hello from the Representer!");
         }
     }
--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
 
 namespace TestRunner
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            MSBuildLocator.RegisterDefaults();
+
+            var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync(@"C:\Programmeren\AnalyzingSourceCodeUsingRoslyn\TestRunner.Exercise\TestRunner.Exercise.csproj");
+            
             Console.WriteLine("Hello from the Test Runner!");
         }
     }
